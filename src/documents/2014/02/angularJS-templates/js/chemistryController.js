bb2380c8dc1fef15d5b5b975fdf6b7b0d7659020
John Ptacek <jptacek@live.com>
2015-02-13 06:22:17 -0600

    Fix up issues with posts

--- src/documents/2014/02/angularJS-templates/js/chemistryController.js
+++ src/documents/2014/02/angularJS-templates/js/chemistryController.js
@@ -2,15 +2,15 @@ chemistryApp.controller('chemTemplateCtrl1',
     function chemCtrl($scope) {
         $scope.periodic = perioicData;
 
-        $scope.template =  {name: 'template1.html', url: 'js/templates/element1.html'};
+        $scope.template =  {name: 'template1.html', url: '/2014/02/angularJS-templates/js/templates/element1.html'};
     }
 ).controller('chemTemplateCtrl2',
     function chemCtrl($scope) {
         $scope.periodic = perioicData;
 
         $scope.templates =
-            [ { name: 'template1.html', url: 'js/templates/element1.html'}
-                , { name: 'template2.html', url: 'js/templates/element2.html'}];
+            [ { name: 'template1.html', url: '/2014/02/angularJS-templates/js/templates/element1.html'}
+                , { name: 'template2.html', url: '/2014/02/angularJS-templates/js/templates/element2.html'}];
         $scope.template = $scope.templates[1];
 
     }
@@ -19,9 +19,9 @@ chemistryApp.controller('chemTemplateCtrl1',
         $scope.periodic = perioicData;
 
         $scope.templates =
-            [ { name: 'template1.html', url: 'js/templates/element1.html'}
-                , { name: 'template2.html', url: 'js/templates/element2.html'}
-                , { name: 'template3.html', url: 'js/templates/element3.html'}];
+            [ { name: 'template1.html', url: '/2014/02/angularJS-templates/js/templates/element1.html'}
+                , { name: 'template2.html', url: '/2014/02/angularJS-templates/js/templates/element2.html'}
+                , { name: 'template3.html', url: '/2014/02/angularJS-templates/js/templates/element3.html'}];
         $scope.templateSelect = $scope.templates[0];
 
         $scope.displayAll =  function() {
